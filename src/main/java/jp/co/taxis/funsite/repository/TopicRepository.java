f0388e8e01b532c313ee2887cad5041d842a3895
User <User@WINDOWS-QK4RSRN>
2022-06-06 09:51:07 +0900

    変更しました。

--- src/main/java/jp/co/taxis/funsite/repository/TopicRepository.java
+++ src/main/java/jp/co/taxis/funsite/repository/TopicRepository.java
@@ -5,13 +5,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import jp.co.taxis.funsite.entity.Player;
+import jp.co.taxis.funsite.entity.Topic;
 import jp.co.taxis.funsite.entity.User;
 
-
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface TopicRepository extends JpaRepository<Topic, Integer> {
 
-	@Query(nativeQuery = true, value = "SELECT * FROM topic WHERE login_id = :loginId")
-	public User getUser(@Param("loginId") String loginId);
+	@Query(nativeQuery = true, value = "SELECT * FROM operation_user WHERE player_id = :player")
+	public User getTopic(@Param("player") Player player);
 
 }
