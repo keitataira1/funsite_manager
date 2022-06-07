9a47d7e41f32717b2ffcac4ea868f5deebce400b
User <User@WINDOWS-Q6A637J>
2022-06-07 11:50:29 +0900

    メッセージプロパティにつけくわえました。

--- src/main/java/jp/co/taxis/funsite/form/PlayerForm.java
+++ src/main/java/jp/co/taxis/funsite/form/PlayerForm.java
@@ -2,12 +2,17 @@ package jp.co.taxis.funsite.form;
 
 import java.time.LocalDate;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 //手直し・追加必要？
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PlayerForm {
 
 	private Integer id;
@@ -15,6 +20,7 @@ public class PlayerForm {
 	@Pattern(regexp = ".{1,10}")
 	private String name;
 
+	@NotNull
 	private LocalDate birthday;
 
 	@Pattern(regexp = ".{1,10}")
