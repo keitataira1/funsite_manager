177c161846e0fdf750385296f051f40ffc733927
User <User@WINDOWS-Q6A637J>
2022-06-07 14:07:47 +0900

    リンクをちょっとなおしました。

--- src/main/java/jp/co/taxis/funsite/controller/PlayerInsertController.java
+++ src/main/java/jp/co/taxis/funsite/controller/PlayerInsertController.java
@@ -15,7 +15,7 @@ import jp.co.taxis.funsite.form.PlayerForm;
 import jp.co.taxis.funsite.service.PlayerInsertService;
 
 @Controller
-@RequestMapping("player")
+@RequestMapping("admin")
 public class PlayerInsertController {
 
 	
@@ -26,7 +26,7 @@ public class PlayerInsertController {
 	 * 入力画面表示メソッド.
 	 * @return input.htmlにリターン
 	 */
-	@RequestMapping(value = "insert/input", method = { RequestMethod.GET, RequestMethod.POST })
+	@RequestMapping(value = "/player/insert/input", method = { RequestMethod.GET, RequestMethod.POST })
 	public String input(@ModelAttribute("player") PlayerForm playerForm) {
 		
 		//入力画面を出すだけ
@@ -37,7 +37,7 @@ public class PlayerInsertController {
 	 * 確認画面表示メソッド.
 	 * @return confirm.htmlにリターン
 	 */
-	@RequestMapping(value = "insert/confirm", method = { RequestMethod.POST })
+	@RequestMapping(value = "/player/insert/confirm", method = { RequestMethod.POST })
 	public String confirm(@ModelAttribute("player") @Validated PlayerForm playerForm, BindingResult result) {
 
 		if (result.hasErrors()) {
@@ -52,7 +52,7 @@ public class PlayerInsertController {
 	 * 登録入力画面（DBに送る）
 	 * @return redirect
 	 */
-	@RequestMapping(value = "insert/insert", method = { RequestMethod.POST })
+	@RequestMapping(value = "/player/insert/insert", method = { RequestMethod.POST })
 	public String insert(@ModelAttribute("player") @Validated PlayerForm playerForm, BindingResult result,
 			RedirectAttributes redirectAttrs) {
 
@@ -81,7 +81,7 @@ public class PlayerInsertController {
 	 * 登録完了画面.
 	 * @return View
 	 */
-	@RequestMapping(value = "insert/complete", method = { RequestMethod.GET })
+	@RequestMapping(value = "/player/insert/complete", method = { RequestMethod.GET })
 	public String complete() {
 		
 		// 画面を表示するだけ
