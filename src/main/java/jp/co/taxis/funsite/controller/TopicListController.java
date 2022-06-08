6e213f28576c48cdaffc9a4b5e1e51e33d72ee89
User <User@WINDOWS-MA9S719>
2022-06-08 10:50:49 +0900

    TopicListControllerの作成

--- src/main/java/jp/co/taxis/funsite/controller/TopicListController.java
+++ src/main/java/jp/co/taxis/funsite/controller/TopicListController.java
@@ -1,34 +1,43 @@
 package jp.co.taxis.funsite.controller;
 
+import java.util.List;
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import jp.co.taxis.funsite.form.TopicForm;
+import jp.co.taxis.funsite.entity.Topic;
+import jp.co.taxis.funsite.service.TopicListService;
 
 @Controller
-@RequestMapping("topic")
+@RequestMapping(value = "admin")
 public class TopicListController {
+
+	@Autowired
+	private TopicListService topicListService;
+
+	@Autowired
+	private MessageSource messageSource;
 
 	/**
 	 * 一覧画面
-	 * 
-	 * @return
 	 */
-	@RequestMapping(value = "list", method = RequestMethod.GET)
-	public String input(@ModelAttribute("topic") TopicForm topicForm) {
-		return "input";
-	}
 
-	@RequestMapping(value = "confirm", method = RequestMethod.GET)
-	public String confirm(@Validated @ModelAttribute("topic") TopicForm topicForm, BindingResult result) {
-		if (result.hasErrors()) {
-			return "confirm";
+	@RequestMapping(value = "topic/list", method = { RequestMethod.GET })
+	public String list(Model model) {
+		List<Topic> topicList = topicListService.selectAll();
+		if (topicList.isEmpty()) {
+			String message = messageSource.getMessage("list.empty.error", null, Locale.getDefault());
+			model.addAttribute("message", message);
 		}
-		return "confirm";
+
+		model.addAttribute("topicList", topicList);
+		return "admin/topic/list";
+
 	}
 
 }
