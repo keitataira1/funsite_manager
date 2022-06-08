0bbf49cec16f6dfcbf3909b4e20882af42f0b69e
User <User@192.168.1.239>
2022-06-08 09:32:13 +0900

    PlayerInsertControllerを変更しました。宮間

--- src/main/java/jp/co/taxis/funsite/controller/PlayerInsertController.java
+++ src/main/java/jp/co/taxis/funsite/controller/PlayerInsertController.java
@@ -1,6 +1,8 @@
 
 package jp.co.taxis.funsite.controller;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -8,7 +10,6 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jp.co.taxis.funsite.entity.Player;
 import jp.co.taxis.funsite.form.PlayerForm;
@@ -55,8 +56,7 @@ public class PlayerInsertController {
 	 * @return redirect
 	 */
 	@RequestMapping(value = "/player/insert/insert", method = { RequestMethod.POST })
-	public String insert(@ModelAttribute("player") @Validated PlayerForm playerForm, BindingResult result,
-			RedirectAttributes redirectAttrs) {
+	public String insert(@ModelAttribute("player") @Validated PlayerForm playerForm, BindingResult result) {
 
 		if (result.hasErrors()) {
 			return "admin/player/insert/input";
@@ -65,23 +65,16 @@ public class PlayerInsertController {
 		// フォームからエンティティへの変換
 		Player player = new Player();
 		player.setId(playerForm.getId());
-		player.setName(playerForm.getName());
-<<<<<<< HEAD
-
-		player.setBirthday(playerForm.getBirthday());
-		
+		player.setName(playerForm.getName());	
 		player.setBirthday(LocalDate.parse(playerForm.getBirthday()));
-=======
->>>>>>> branch 'master' of git@github.com:keitataira1/funsite_manager.git
 		player.setComment(playerForm.getComment());
 
 		// 登録処理
-		player = playerInsertService.insert(player);
+		playerInsertService.insert(player);
 		playerForm.setId(player.getId());
 
-		redirectAttrs.addFlashAttribute("player", playerForm);
 
-		return "redirect:complete";
+		return "admin/player/insert/complete";
 	}
 
 	/**
