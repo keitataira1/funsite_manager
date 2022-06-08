f83a85af283e6dc61c71757e9ab1375d4306ffd9
User <User@192.168.1.239>
2022-06-08 12:51:43 +0900

    選手更新のコントローラーとhtmlとlist.htmlを変更しました。宮間

--- src/main/java/jp/co/taxis/funsite/service/PlayerUpdateService.java
+++ src/main/java/jp/co/taxis/funsite/service/PlayerUpdateService.java
@@ -3,17 +3,15 @@ package jp.co.taxis.funsite.service;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.stereotype.Service;
 
 import jp.co.taxis.funsite.entity.Player;
-import jp.co.taxis.funsite.exception.ApplicationException;
 import jp.co.taxis.funsite.repository.PlayerRepository;
 
 @Transactional
 @Service
 public class PlayerUpdateService {
-	
+
 	@Autowired
 	private PlayerRepository playerRepository;
 
@@ -21,13 +19,10 @@ public class PlayerUpdateService {
 		Player player = playerRepository.findById(id).orElse(null);
 		return player;
 	}
-	
-	public void update(Player player) {
-		try {
-			playerRepository.save(player);
-		} catch (OptimisticLockingFailureException e) {
-			throw new ApplicationException("optimistic.locking.error");
-		}
 
-}
+	public void update(Player player) {
+
+		playerRepository.save(player);
+
 	}
+}
