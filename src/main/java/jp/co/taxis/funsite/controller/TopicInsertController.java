1ae44c80f0cf5ab24fbbf90cc1939d2c998dba4e
User <User@WINDOWS-QK4RSRN>
2022-06-09 17:47:23 +0900

    TopicInsertControllerの変更（一覧の名前を表示できました）

--- src/main/java/jp/co/taxis/funsite/controller/TopicInsertController.java
+++ src/main/java/jp/co/taxis/funsite/controller/TopicInsertController.java
@@ -1,16 +1,24 @@
 package jp.co.taxis.funsite.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import jp.co.taxis.funsite.entity.PlayerEntity;
 import jp.co.taxis.funsite.entity.TopicEntity;
 import jp.co.taxis.funsite.form.TopicForm;
+import jp.co.taxis.funsite.service.PlayerListService;
 import jp.co.taxis.funsite.service.TopicInsertService;
+import jp.co.taxis.funsite.service.TopicListService;
+import jp.co.taxis.funsite.service.TopicUpdateService;
 
 @Controller
 @RequestMapping("admin")
@@ -18,6 +26,15 @@ public class TopicInsertController {
 
 	@Autowired
 	private TopicInsertService topicInsertService;
+	
+	@Autowired
+	private TopicListService topicListService;
+
+	@Autowired
+	private PlayerListService playerListService;
+
+	@Autowired
+	private TopicUpdateService topicUpdateService;
 
 	/**
 	 * 入力画面表示メソッド.
@@ -25,9 +42,17 @@ public class TopicInsertController {
 	 * @return input.htmlにリターン
 	 */
 	@RequestMapping(value = "/topic/insert/input", method = { RequestMethod.GET, RequestMethod.POST })
-	public String input(@ModelAttribute("topic") TopicForm topicForm) {
+	public String input(@RequestParam("id") Integer playerId, Model model,
+			@ModelAttribute("topic") TopicForm topicForm) {
 
 		// 入力画面を出すだけ
+		List<TopicEntity> topicList = topicListService.selectAll();
+		List<PlayerEntity> playerList = playerListService.selectAll();
+		TopicEntity topicEntity = topicUpdateService.getTopic(playerId);
+
+		model.addAttribute("topicList", topicList);
+		model.addAttribute("playerList", playerList);
+		model.addAttribute("name", topicEntity.getPlayer());
 		return "admin/topic/insert/input";
 	}
 
