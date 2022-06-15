cae393f3b8e551ef9e92fdb153ae634986fbb63a
User <User@WINDOWS-Q6A637J>
2022-06-15 18:04:01 +0900

    同じ名前が登録できないようにPlayerInsertServiceとControllerをなおしました。

--- src/main/java/jp/co/taxis/funsite/controller/PlayerInsertController.java
+++ src/main/java/jp/co/taxis/funsite/controller/PlayerInsertController.java
@@ -2,8 +2,10 @@
 package jp.co.taxis.funsite.controller;
 
 import java.time.LocalDate;
+import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jp.co.taxis.funsite.entity.PlayerEntity;
+import jp.co.taxis.funsite.exception.ApplicationException;
 import jp.co.taxis.funsite.form.PlayerForm;
 import jp.co.taxis.funsite.service.PlayerInsertService;
 
@@ -22,6 +25,9 @@ public class PlayerInsertController {
 
 	@Autowired
 	private PlayerInsertService playerInsertService;
+	
+	@Autowired
+	MessageSource messageSource;
 
 	/**
 	 * 入力画面表示メソッド.
@@ -74,8 +80,18 @@ public class PlayerInsertController {
 		player.setImage(playerForm.getImage());
 
 		// 登録処理
-		playerInsertService.insert(player);
-		playerForm.setId(player.getId());
+		
+		try {
+			// 更新処理
+			playerInsertService.insert(player);
+			playerForm.setId(player.getId());
+		} catch (ApplicationException e) {
+			String messageKey = e.getMessage();
+			String message = messageSource.getMessage(messageKey, null, Locale.getDefault());
+			redirectAttrs.addFlashAttribute("message", message);
+			return "redirect:../list";
+		}
+		
 
 		redirectAttrs.addFlashAttribute("player", playerForm);
 		return "redirect:complete";
--- src/main/java/jp/co/taxis/funsite/service/PlayerInsertService.java
+++ src/main/java/jp/co/taxis/funsite/service/PlayerInsertService.java
@@ -1,10 +1,13 @@
 package jp.co.taxis.funsite.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import jp.co.taxis.funsite.entity.PlayerEntity;
+import jp.co.taxis.funsite.exception.ApplicationException;
 import jp.co.taxis.funsite.repository.PlayerRepository;
 
 @Transactional
@@ -15,6 +18,10 @@ public class PlayerInsertService {
 	private PlayerRepository playerRepository;
 
 	public PlayerEntity insert(PlayerEntity player) {
+		List<PlayerEntity> searchList = playerRepository.searchSameName(player.getName());
+		if (!searchList.isEmpty()) {
+			throw new ApplicationException("player.samename.error");
+		}
 		player.setVersion(1);
 		PlayerEntity result = playerRepository.save(player);
 		return result;
